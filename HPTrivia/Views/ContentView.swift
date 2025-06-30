36350e261e61c581857579848becc5d017dbee0d
vladys <wawladyslaw@gmail.com>
2025-06-30 11:02:36 +0300

    הוספנו אנימציה לכותרת טריוויה

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animateViewsIn = false //יצרנו משתנה כבוי לאנימציה
+    
     var body: some View {
         //גאו מתאים את האפליקציה לגודל המסך
         GeometryReader { geo in
@@ -23,10 +25,37 @@ struct ContentView: View {
                             .linear(duration: 60)
                     }//פייס זו האניצמייית תזוזה של הרקע
 
+                VStack {
+                    VStack {//זה כדי שנוכל להוסיף תצוגה לכל מה שזה עוטף
+                        if animateViewsIn { //אם האנימייט טרו אז לבצע
+                            VStack {
+                                Image(systemName: "bolt.fill")
+                                    .imageScale(.large)
+                                    .font(.largeTitle)
+                                
+                                Text("HP")
+                                    .font(.custom("PartyLetPlain", size: 70))
+                                    .padding(.bottom, -50)
+                                
+                                Text("Trivia")
+                                    .font(.custom("PartyLetPlain", size: 60))
+                            }
+                            .padding(.top, 70)
+                            .transition(.move(edge: .top))
+                        }//זה הסוג של האנימציה שאנו רוצים
+                    }
+                    .animation(.easeInOut(duration: 0.7).delay(2), value: animateViewsIn)//זו האנימציה עצמה
+                    
+                    Spacer()
+                }
+                
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear { //כאן אנו מפעילים את המשתנה שהגדרנו למעלה
+                animateViewsIn = true
+        }
     }
 }
 
