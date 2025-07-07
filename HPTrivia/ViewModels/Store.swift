17804a036623721420d37c6d5984ff88542c60b1
vladys <wawladyslaw@gmail.com>
2025-07-07 12:40:35 +0300

    יצרנו פונקציה לרכישה שתבדוק את הרכישה לפני סיום התשלום ואז אם הוא תקין לעביר ולסיים את הרכישה בנוסף השתמשנו בברייק כדי לעצור את הרכישה במידה ויש איזשהו חסם

--- HPTrivia/ViewModels/Store.swift
+++ HPTrivia/ViewModels/Store.swift
@@ -14,7 +14,7 @@ class Store {
     var products : [Product] = []
     //יצרנו משתנה של קנויים שהוא קבוצה סט של מחרוזות ריקה בהתחלה
     //סט מאפשר לשמור פריט רק פעם אחת
-    //זאת אומרת אחרי שקנית אותו אים אפשר לקנות אותו דבר שוב
+    //זאת אומרת אחרי שקנית אותו אי אפשר לקנות אותו דבר שוב
     var purchased = Set<String>()
     //יצרנו משתנה בשם עדכונים
     //סוג המשתנה הוא טאסק משימה אסינכרוני. וויד לא מחזיר ערך. נבר לעולם לא נכשל. ניל זהו ערך התחלתי כלומר בהתחלה אין טאסק משימה
@@ -39,7 +39,45 @@ class Store {
         }
     }
     //Purchase a product
-    
+    //הפונקצייה מקבלת פרמטר אחד מסוג פרודוקט ומנסה לבצע רכישה איתו ברקע עם אסינק
+    func purchase(_ product: Product) async {
+        do {
+            //הגדרנו שינסה לקנות את המוצר
+            let result = try await product.purchase()
+            //אלו האפשרויות מה יקרה לאחר ניסיון קניה
+            switch result {
+            //הקניה הצליחה. וצריך לאשר את הקבלה והעברת הכסף
+            case .success(let verificationResult):
+                //הגדרנו סוגים של אישורים כדי שיבדוק אם האישור אמיתי או מזויף
+                switch verificationResult {
+                //אם האישור לא מאומת או מזויף
+                case .unverified(let signedType, let verificationError):
+                    //מדווחים על שגיאה  ועוצרים את הרכישה
+                    print("Error on \(signedType): \(verificationError)")
+                //אם האישור מאומת ואמיתי
+                case .verified(let signedType):
+                    //מעבירים אותה לאפל לסיום הרכישה
+                    purchased.insert(signedType.productID)
+                //מודיע לאפ סטור שסיימנו לבדוק את הרכישה כדי שלא תשאר תלויה
+                    await signedType.finish()
+                }
+                
+            //המשתמש ביטל את הקניה או ההורים לא אישרו לילד את הקניה
+            case .userCancelled:
+                break //הגדרנו שפשוט לא יעשה כלום
+                
+            //מחכה לאישור הקניה כמו ילד שמחכה לאישור מההורים
+            case .pending:
+                break //הגדרנו שפשוט לא יעשה כלום
+                
+            //זה ברירת מחדל לכל אופציה אחרת
+            @unknown default: //כברירת מחדל הגדרנו לשבור את הסוויצ ולא לעשות כלום
+                break
+            }
+        } catch {
+            print("Unable to purchase product: \(error)")
+        }
+    }
     //Chack for purchased products
     
     //Connect with App Store to watch for purchase and transaction update
